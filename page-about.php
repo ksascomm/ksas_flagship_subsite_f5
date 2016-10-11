9f2105e4d5c9a531ba94e88bd42457510d218a27
ksascomm <ksasweb@jhu.edu>
2016-10-11 11:10:43 -0400

    remove unnecessary <section>, <h2> now <h1>

--- page-about.php
+++ page-about.php
@@ -3,16 +3,14 @@
 <?php locate_template('/parts/nav-sidebar.php', true, false); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="small-12 medium-8 medium-pull-4 columns wrapper radius-left offset-topgutter">
-		<?php locate_template('/parts/nav-breadcrumbs.php', true, false); ?>	
-		<section>
-				<h2><?php the_title();?></h2>
-					<?php if ( has_post_thumbnail()) { ?> 
-						<div class="floatright">
-							<?php the_post_thumbnail('full',array('class'	=> "radius-topleft")); ?>
-						</div>
-					<?php } ?>
-				<?php the_content(); ?>
-		</section>
+		<?php locate_template('/parts/nav-breadcrumbs.php', true, false); ?>			
+			<h1><?php the_title();?></h1>
+				<?php if ( has_post_thumbnail()) { ?> 
+					<div class="floatright">
+						<?php the_post_thumbnail('full',array('class'	=> "radius-topleft")); ?>
+					</div>
+				<?php } ?>
+			<?php the_content(); ?>
 	</div>	<!-- End main content (left) section -->
 <?php endwhile; endif; ?>	
 </div> 
