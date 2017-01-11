4cfb77d7ede4538e9fb4dfc3bd57be928cc93450
ksascomm <ksasweb@jhu.edu>
2017-01-11 15:14:42 -0500

    schema & aria roles edit

--- parts/nav-sidebar.php
+++ parts/nav-sidebar.php
@@ -1,14 +1,15 @@
-	<aside class="small-12 large-4 columns hide-for-print" id="sidebar"> 
+	<aside class="small-12 large-4 columns hide-for-print" id="sidebar" itemscope="itemscope" itemtype="http://schema.org/WPSideBar">  
 		<div class="blue_bg radius-topright offset-gutter" id="sidebar_header">
 			<h5 class="white"><a href="<?php echo site_url(); ?>" title="<?php bloginfo('name'); ?>" class="white"><?php bloginfo('name'); ?></a></h5>
 		</div>
 		<!--Begin Site Navigation Links -->
-		<div class="row" role="navigation">
+		
 			<?php wp_nav_menu( array( 
 				'theme_location' => 'main_nav', 
 				'menu_class' => 'nav', 
 				'fallback_cb' => 'foundation_page_menu', 
 				'depth' => 2 )); ?> 
-		</div> <!--End Site Navigation Links -->
+		
+		<!--End Site Navigation Links -->
 		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('under-nav-sb') ) : ?><?php endif; ?>
 	</aside> <!-- End Sidebar -->
