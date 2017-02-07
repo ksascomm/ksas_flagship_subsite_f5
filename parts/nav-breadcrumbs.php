7246c988ffe3fdb7058f785c1fbd0aa1167be9be
ksascomm <ksasweb@jhu.edu>
2017-02-07 14:23:38 -0500

    added aria-label to nav

--- parts/nav-breadcrumbs.php
+++ parts/nav-breadcrumbs.php
@@ -2,13 +2,15 @@
 	$home_url = home_url();
 	$theme_option = flagship_sub_get_global_options();	
 	
-	if ( $theme_option['flagship_sub_breadcrumbs']  == '1' ) { 
-	wp_nav_menu( array( 
-				'container' => 'nav',
+	if ( $theme_option['flagship_sub_breadcrumbs']  == '1' ) : ?>
+		<nav aria-label="breadcrumbs">
+			<?php wp_nav_menu( array( 
+				'container' => 'false',
 				'container_class' => 'offset-topgutter',
 				'theme_location' => 'main_nav',
 				'menu_class' => 'breadcrumbs',
 				'items_wrap' => '<ul id="%1$s" class="%2$s"><li><a href="' . $home_url . '">' . $theme_option['flagship_sub_breadcrumb_home'] . '</a></li>%3$s</ul>',
-				'walker'=> new flagship_bread_crumb )); 
-	}
-?>				
+				'walker'=> new flagship_bread_crumb )
+			); ?>
+		</nav>
+	<?php endif; ?>
